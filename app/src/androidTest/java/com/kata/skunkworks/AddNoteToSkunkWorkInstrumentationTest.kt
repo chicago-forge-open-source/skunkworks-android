b7f5f402160559accc135710a8e832f9dfcd9b31
david <dschinkel@gmail.com>
2019-03-19 13:52:14 -0500

    remote test implementation that is WIP so we do not get compile errors

--- app/src/androidTest/java/com/kata/skunkworks/AddNoteToSkunkWorkInstrumentationTest.kt
+++ app/src/androidTest/java/com/kata/skunkworks/AddNoteToSkunkWorkInstrumentationTest.kt
@@ -1,13 +1,10 @@
 package com.kata.skunkworks
 
 import android.view.View
-import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.intent.rule.IntentsTestRule
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.After
@@ -35,7 +32,7 @@ class AddNoteToSkunkWorkInstrumentationTest{
 
     @Test
     fun showsASkunkWorkWithAddNotesField() {
-        val skunkWorkList = onView(withId(R.id.skunk_works_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()))
+//        val skunkWorkList = onView(withId(R.id.skunk_works_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()))
 
     }
 
